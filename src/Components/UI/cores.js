d155e49e366417f35aa98601cdbf69c282d19ebc
matheus <mat.feijo2506@gmail.com>
2022-02-10 20:19:33 -0400

    tela de login

---
+++ src/Components/UI/cores.js
@@ -0,0 +1,10 @@
+export const fundoEscuro = "rgba(0,0,0)";
+export const fundoClaro = "#fff";
+
+export const temaEscuro ={
+    body:fundoEscuro,
+}
+
+export const temaClaro ={
+    body:fundoClaro,
+}
---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import {TextField,Button,FormControl,Input, Typography, FormHelperText} from "@material-ui/core";
+import "./login.css";
+
+
+export const Login = () =>{
+
+    const Container = styled.div`
+        background-color:#025959;
+        min-height:100vh;
+    `;
+
+
+    const initalValues ={
+        user:"",
+        senha:"",
+    }
+
+    const [formValues,setFormValues] = useState(initalValues);
+    const [formErrors,setFormErrors] = useState({});
+    const [isSubmiting,setIsSubmiting] = useState(false);
+
+    const submit = () =>{
+        
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        setFormErrors(validation(formValues));
+        setIsSubmiting(true);
+    }
+
+    const validation = (values) =>{
+        let errors = {}
+
+        if(!values.user){
+            errors.user = "Campo Obrigatorio"
+        }
+        if(!values.user){
+            errors.senha = "Campo Obrigatorio"
+        }
+
+        return errors;
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value });
+    }
+
+    useEffect(()=>{
+        if(Object.keys(formErrors) && isSubmiting){
+            submit();
+            setIsSubmiting(false);
+        }
+    },[formErrors,isSubmiting,submit])
+
+    return(
+        <div className="container__login" style={{
+            height: "100vh",
+            backgroundColor:"#4B1A73"
+        }}>
+            <div style={{
+                justifyContent:"center",
+                display: "grid",
+                position: "relative",
+                top:"20vh",
+                backgroundColor:"#D5D9DA",
+                padding: "250px 5px 50px 5px",
+                width: "50vh",
+                left: "70vh",
+                borderRadius:"60px",
+                border:"4px solid #471C75",
+                
+
+            }}>
+                <Typography variant="h3" style={{position:"relative",bottom:"10vh",color:"#431D74"}} >Login Carteira</Typography>
+                <TextField
+                    value={formValues.user}
+                    name="user"
+                    id="user"
+                    label="Usuario"
+                    variant="outlined"
+                    margin="normal"
+                    type="text"
+                    fullWidth
+                    error={formErrors.user}
+                    helperText={formErrors.user}
+                    onChange={handleChange} 
+                   style={{
+                       width: 310,
+                   }}
+                />
+                
+                <TextField
+                    value={formValues.senha}
+                    name="senha"
+                    id="senha"
+                    label="Senha"
+                    type="password"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    error={formErrors.senha}
+                    helperText={formErrors.senha}
+                    onChange={handleChange} 
+                
+                />
+                <a 
+                    href="/forgetPass" 
+                    style={{
+                        display: "block",
+                        paddingBottom:"40px",
+                    }}
+                >Esqueci minha senha</a>
+
+                <Button 
+                    variant="contained"
+                    disableRipple
+                    style={{
+                        display: "block",
+                        color: "#fff",
+                        backgroundColor:"#431D74",
+                        fontWeight:600
+                    }}
+                    onClick={handleSubmit}
+                > Confirmar</Button>
+            </div>
+
+            </div>
+
+    )
+}
